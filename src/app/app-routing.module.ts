0b261c1969f2d62706a3d7b8d0746e5af63fb772
Oladele Oluwanishola <149659874+Oluwanishola12@users.noreply.github.com>
2024-05-20 22:18:25 +0100

    MONDAY

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutComponent } from './about/about.component';
+import { HomeComponent } from './home/home.component';
+import { ContactComponent } from './contact/contact.component';
+import { ServicesComponent } from './services/services.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+
+{ path:'home', component: HomeComponent},
+{ path:'about', component: AboutComponent},
+{ path:'contact', component: ContactComponent},
+{ path:'services', component: ServicesComponent}
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
