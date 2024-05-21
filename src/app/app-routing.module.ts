a4e0468b232a9ec433449ddaa17de077cf294cd1
Oladele Oluwanishola <149659874+Oluwanishola12@users.noreply.github.com>
2024-05-21 10:10:51 +0100

    TUESDAY

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,6 +7,7 @@ import { ServicesComponent } from './services/services.component';
 
 const routes: Routes = [
 
+{ path:'', component: HomeComponent},
 { path:'home', component: HomeComponent},
 { path:'about', component: AboutComponent},
 { path:'contact', component: ContactComponent},
